7026e5dbc0a73842018a5055828bdfa6f37cb8f8
Chinmay Pareek <cpareek@opentext.com>
2024-01-10 00:29:25 +0530

    Adding routes and components

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,30 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countriesUnique = new Set(
+    cities.map((city) =>
+      JSON.stringify({ country: city.country, emoji: city.emoji })
+    )
+  );
+  const countries = [...countriesUnique].map((each) => JSON.parse(each));
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => {
+        return <CountryItem country={country} key={country.emoji} />;
+      })}
+    </ul>
+  );
+}
+
+export default CountryList;
